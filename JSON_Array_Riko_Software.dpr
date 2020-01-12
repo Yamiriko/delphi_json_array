8cb102c64af9da84441680f600244b8f1763dde4
Yamiriko <riko.kurniawan18@yahoo.co.id>
2020-01-12 17:39:54 +0700

    first commit

---
+++ JSON_Array_Riko_Software.dpr
@@ -0,0 +1,14 @@
+program JSON_Array_Riko_Software;
+
+uses
+  Vcl.Forms,
+  Unit1 in 'Unit1.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
